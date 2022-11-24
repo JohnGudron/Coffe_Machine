7689e586dfb138b6313eef3c1de750034f81157f
JohnGudron <oak43556@gmail.com>
2022-11-24 22:02:58 +0300

    Code for 5th stage of project. "remaining" and "exit" options have been added. Resources was wrapped into a data class.

--- src/Programm.kt
+++ src/Programm.kt
@@ -1,90 +1,135 @@
+data class Resources(
+    var water: Int = 400,
+    var milk: Int = 540,
+    var beans: Int = 120,
+    var cups: Int = 9,
+    var money: Int = 550
+)
+
 fun main() {
-    var water = 400
-    var milk = 540
-    var beans = 120
-    var cups = 9
-    var money = 550
-    var text = { water: Int, milk: Int, beans: Int, cups: Int, money: Int ->
+    val state = Resources()
+    val costs = arrayListOf(
+        intArrayOf(250, 0, 16, 1),  // espresso
+        intArrayOf(350, 75, 20, 1), // latte
+        intArrayOf(200, 100, 12, 1) // cappuccino
+    )
+    val text = { _water: Int, _milk: Int, _beans: Int, _cups: Int, _money: Int ->
         println(
             """The coffee machine has:
-$water ml of water
-$milk ml of milk
-$beans g of coffee beans
-$cups disposable cups
-$$money of money"""
+$_water ml of water
+$_milk ml of milk
+$_beans g of coffee beans
+$_cups disposable cups
+$$_money of money"""
         )
     }
-    text(water, milk, beans, cups, money)
-    println("Write action (buy, fill, take):")
-    val action = readln()
+
+    fun check(cost: IntArray): Pair<Boolean, String> {
+        return when {
+            state.water - cost[0] < 0 -> Pair(false, "water")
+            state.milk - cost[1] < 0 -> Pair(false, "milk")
+            state.beans - cost[2] < 0 -> Pair(false, "beans")
+            state.cups - cost[3] < 0 -> Pair(false, "cups")
+            else -> Pair(true, "full")
+        }
+    }
+
     fun buy() {
-        println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+        println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
         when (readln()) {
             "1" -> {
-                water -= 250
-                beans -= 16
-                money += 4
-                cups -= 1
-                text(water, milk, beans, cups, money)
+                val act = check(costs[0])
+                if (act.first) {
+                    println("I have enough resources, making you a coffee!")
+                    state.water -= 250
+                    state.beans -= 16
+                    state.money += 4
+                    state.cups -= 1
+                } else {
+                    println("Sorry, not enough ${act.second}!")
+                }
             }
 
             "2" -> {
-                water -= 350
-                milk -= 75
-                beans -= 20
-                money += 7
-                cups -= 1
-                text(water, milk, beans, cups, money)
+                val act = check(costs[1])
+                if (act.first) {
+                    println("I have enough resources, making you a coffee!")
+                    state.water -= 350
+                    state.milk -= 75
+                    state.beans -= 20
+                    state.money += 7
+                    state.cups -= 1
+                } else {
+                    println("Sorry, not enough ${act.second}!")
+                }
             }
 
             "3" -> {
-                water -= 200
-                milk -= 100
-                beans -= 12
-                money += 6
-                cups -= 1
-                text(water, milk, beans, cups, money)
+                val act = check(costs[2])
+                if (act.first) {
+                    println("I have enough resources, making you a coffee!")
+                    state.water -= 200
+                    state.milk -= 100
+                    state.beans -= 12
+                    state.money += 6
+                    state.cups -= 1
+                } else {
+                    println("Sorry, not enough ${act.second}!")
+                }
+            }
+
+            "back" -> { // no action in this branch for now
             }
         }
-
     }
 
     fun fill() {
         println("Write how many ml of water you want to add:")
-        water += readln().toInt()
+        state.water += readln().toInt()
         println("Write how many ml of milk you want to add:")
-        milk += readln().toInt()
+        state.milk += readln().toInt()
         println("Write how many grams of coffee beans you want to add:")
-        beans += readln().toInt()
+        state.beans += readln().toInt()
         println("Write how many disposable cups you want to add: ")
-        cups += readln().toInt()
-        text(water, milk, beans, cups, money)
+        state.cups += readln().toInt()
     }
 
     fun take() {
-        println("I gave you $$money")
-        money -= money
-        text(water, milk, beans, cups, money)
+        println("I gave you $${state.money}")
+        state.money -= state.money
     }
 
-    when (action) {
-        "buy" -> buy()
-        "fill" -> fill()
-        "take" -> take()
+    fun remain() {
+        text(state.water, state.milk, state.beans, state.cups, state.money)
     }
 
-    // println("Write how many ml of water the coffee machine has:")
-    // val water = readln().toInt()
-    // println("Write how many ml of milk the coffee machine has:")
-    // val milk = readln().toInt()
-    // println("Write how many grams of coffee beans the coffee machine has:")
-    // val beans = readln().toInt()
-    // println("Write how many cups of coffee you will need:")
-    // val cup = readln().toInt()
-    // val n = intArrayOf(water / 200, milk / 50, beans / 15).minOrNull()
-    // if (n!! == cup) {
-    //     println("Yes, I can make that amount of coffee")
-    // } else if (n > cup) {
-    //     println("Yes, I can make that amount of coffee (and even ${n - cup} more than that)")
-    // } else println("No, I can make only $n cups of coffee")
+    fun exit() {
+        // just empty function for now
+    }
+
+    fun menu() {
+        println("Write action (buy, fill, take, remaining, exit):")
+        val action = readln()
+        when (action) {
+            "buy" -> {
+                buy()
+                menu()
+            }
+            "fill" -> {
+                fill()
+                menu()
+            }
+            "take" -> {
+                take()
+                menu()
+            }
+            "remaining" -> {
+                remain()
+                menu()
+            }
+            "exit" -> exit()
+        }
+    }
+
+    menu()
 }
